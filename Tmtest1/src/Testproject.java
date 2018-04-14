2fc3d4f1644ad1803be1772247a4f10156372729
abhi2255 <abhi5bannu@gmail.com>
2018-04-13 20:06:23 -0500

    3rd

--- Tmtest1/src/Testproject.java
+++ Tmtest1/src/Testproject.java
@@ -1,8 +1,15 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Testproject {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://www.facebook.com");
+		driver.manage().window().maximize();
+		
+		
 
 	}
 
